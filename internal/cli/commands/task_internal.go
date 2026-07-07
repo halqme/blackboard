ade901dba1e6bffc68a806619101c9b653fac070
HAL <68320771+halqme@users.noreply.github.com>
2026-07-07 18:21:33 +0900

    Use atomic updates for internal task commands

--- internal/cli/commands/task_internal.go
+++ internal/cli/commands/task_internal.go
@@ -9,23 +9,27 @@ import (
 )
 
 func CmdPauseActiveTask(_ []string, s store.Store, st store.State) error {
-	pauseActiveTask(&st)
-	st.Revision++
-	if err := s.Save(st); err != nil {
-		return err
-	}
-	return nil
+	_, err := updateState(s, st.Revision, func(current *store.State) error {
+		pauseActiveTask(current)
+		return nil
+	})
+	return err
 }
 
 func CmdCreateTask(args []string, s store.Store, st store.State) error {
 	if len(args) < 1 {
 		return commandkit.Usage("task title is required")
 	}
-	task := createTask(&st, strings.Join(args, " "))
-	st.Revision++
-	if err := s.Save(st); err != nil {
+
+	var task store.Task
+	updated, err := updateState(s, st.Revision, func(current *store.State) error {
+		task = createTask(current, strings.Join(args, " "))
+		return nil
+	})
+	if err != nil {
 		return err
 	}
-	fmt.Fprintf(commandkit.Out, "created task %s at revision v%d\n", task.ID, st.Revision)
+
+	fmt.Fprintf(commandkit.Out, "created task %s at revision v%d\n", task.ID, updated.Revision)
 	return nil
 }
