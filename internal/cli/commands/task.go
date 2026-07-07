228baa327134f993b99454ccde20314864267f0a
HAL <68320771+halqme@users.noreply.github.com>
2026-07-07 18:20:17 +0900

    Use atomic updates for task creation

--- internal/cli/commands/task.go
+++ internal/cli/commands/task.go
@@ -12,13 +12,17 @@ func CmdTaskNew(args []string, s store.Store, st store.State) error {
 	if len(args) < 1 {
 		return commandkit.Usage("task title is required\n\nNext action:\n  - Try 'bb help task new' for command usage.")
 	}
-	pauseActiveTask(&st)
-	createTask(&st, strings.Join(args, " "))
-	st.Revision++
-	if err := s.Save(st); err != nil {
+
+	updated, err := updateState(s, st.Revision, func(current *store.State) error {
+		pauseActiveTask(current)
+		createTask(current, strings.Join(args, " "))
+		return nil
+	})
+	if err != nil {
 		return err
 	}
-	fmt.Fprintf(commandkit.Out, "created task %s at revision v%d\n", st.Tasks[len(st.Tasks)-1].ID, st.Revision)
+
+	fmt.Fprintf(commandkit.Out, "created task %s at revision v%d\n", updated.Tasks[len(updated.Tasks)-1].ID, updated.Revision)
 	return nil
 }
 
